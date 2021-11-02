45dcf969bc2c4bcc2a8c1030e1cac0f8fe7cf939
stanhaakman <stanhaakman@icloud.com>
2021-11-02 15:07:10 +0100

    Setup dashboard to recieve the wanted data and convert to dataframe. Also finish the first form.

--- webapp/static/js/predict.js
+++ webapp/static/js/predict.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 	$('.submit-button').click(function(){
-		const test1 = $('#test1').val();
-		const test2 = $('#test2').val();
+		const stm_km_tot_mld = $('#stm_km_tot_mld').val();
+		const stm_km_van_mld = $('#stm_km_van_mld').val();
+		const smt_reactie_duur = $('#smt_reactie_duur').val();
+		const meldtijd = $('#meldtijd').val();
+		const stm_techn_mld = $('stm_techn_mld').val();
+		const stm_prioriteit = $('stm_prioriteit').val();
+		const Oorzaak = $('Oorzaak').val();
+		const stm_equipm_soort_mld = $('stm_equipm_soort_mld').val();
+		const traject = $('traject').val();
+		const stm_contractgeb_gst = $('stm_contractgeb_gst').val();
 		$.ajax({
 			url: '/',
 			data: $('form').serialize(),
